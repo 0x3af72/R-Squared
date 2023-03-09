fd3207da0a56f66e8843de4d5052ff84d6902da6
56cy <88962938+56cy@users.noreply.github.com>
2023-03-09 15:46:35 +0800

    driver neatness

--- scrape.py
+++ scrape.py
@@ -18,7 +18,7 @@ from printswitch import PRINTS
 
 # constants
 EXECUTABLE_PATH = "chromedriver.exe"
-BRAVE_PATH = "drivers/brave.exe"
+BRAVE_PATH = "C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe" # have to install brave for this to work
 POST_ELEMENT_CLASS = "Post "
 POST_TEXT_CLASS = "SQnoC3ObvgnGjWt90zD9Z _2INHSNB8V5eaWp4P0rY_mE"
 POST_DESCRIPTION_CLASS = "_3xX726aBn29LDbsDtzr_6E _1Ap4F5maDtT1E1YuCiaO0r D3IL3FD0RFy_mkKLPwL4"
@@ -82,108 +82,114 @@ def get_posts_PTC(
     WAIT_TIMEOUT=20, ignore=[], logging=True,
 ):
     
-    # set logging switch
-    printswitch.switch.print = logging
+    try:
     
-    # just in case, create screenshots folder
-    try: os.mkdir("screenshots")
-    except FileExistsError: pass
+        # set logging switch
+        printswitch.switch.print = logging
+        
+        # just in case, create screenshots folder
+        try: os.mkdir("screenshots")
+        except FileExistsError: pass
 
-    # setup driver
-    driver = setup_driver(subreddit)
+        # setup driver
+        driver = setup_driver(subreddit)
 
-    # wait until enough posts are loaded
-    start_time = time.time()
-    post_elems = []
-    while len(post_elems) < max_posts:
-        post_elems = [
-            element for element in
-            driver.find_elements(By.CSS_SELECTOR, f"[class*='{POST_ELEMENT_CLASS}']")
-            if not to_ignore(element, ignore)
-        ]
-        if time.time() - start_time >= WAIT_TIMEOUT: break
-
-    # find post: titles and links
-    posts = []
-    for element in post_elems:
-
-        # exit if limit reached
-        if len(posts) == max_posts: break
-
-        # ads and anomaly skipper
-        try:
-            text_element = element.find_element(By.XPATH, f".//a[@class='{POST_TEXT_CLASS}']")
-        except selenium.common.exceptions.WebDriverException: # this should skip ads
-            continue
-
-        # save screenshot and tts
-        path = "screenshots/" + "".join(random.choice(string.ascii_letters + string.digits) for i in range(32))
-        driver.execute_script('arguments[0].scrollIntoView({block: "center"});', element) # prevent cutting off
-        element.screenshot(path + ".png")
-        save_tts(text_element.text, path + ".wav")
-
-        # add the new element to posts
-        new_element = {
-            "title": text_element.text[:92],
-            "link": text_element.get_attribute("href"),
-            "comments": [],
-            "thumbnail": path,
-        }
-        posts.append(new_element)
-    
-    # get top comments from posts
-    for idx, post in enumerate(posts):
-
-        # debug message
-        PRINTS(f"[DEBUG] Scraping post: {post['title']} ({idx + 1} / {len(posts)})")
-
-        # change webpage
-        driver.get(post["link"])
-
-        # wait until enough comments are loaded
+        # wait until enough posts are loaded
         start_time = time.time()
-        comment_elems = []
-        while len(comment_elems) < max_videos * max_comments:
-            comment_elems = [
+        post_elems = []
+        while len(post_elems) < max_posts:
+            post_elems = [
                 element for element in
-                driver.find_elements(By.CSS_SELECTOR, f"[class^='{COMMENT_ELEMENT_CLASS}']")
-                if "level 1" in element.text # only get main posts
+                driver.find_elements(By.CSS_SELECTOR, f"[class*='{POST_ELEMENT_CLASS}']")
+                if not to_ignore(element, ignore)
             ]
             if time.time() - start_time >= WAIT_TIMEOUT: break
 
-        # screenshot and store each comment
-        cur_comments = []
-        for element in comment_elems:
+        # find post: titles and links
+        posts = []
+        for element in post_elems:
 
             # exit if limit reached
-            if len(post["comments"]) == max_videos: break
+            if len(posts) == max_posts: break
 
+            # ads and anomaly skipper
             try:
-                # get comment as text
-                text_element = element.find_element(By.XPATH, f".//div[@class='{COMMENT_TEXT_CLASS}']")
-                if not text_element.text:
-                    continue
+                text_element = element.find_element(By.XPATH, f".//a[@class='{POST_TEXT_CLASS}']")
+            except selenium.common.exceptions.WebDriverException: # this should skip ads
+                continue
 
-                # save screenshot and tts
-                path = "screenshots/" + "".join(random.choice(string.ascii_letters + string.digits) for i in range(32))
-                driver.execute_script('arguments[0].scrollIntoView({block: "center"});', element) # prevent cutting off
-                element.screenshot(path + ".png")
-                comment_text = filter_urls(text_element.text)
-                save_tts(comment_text, path + ".wav")
+            # save screenshot and tts
+            path = "screenshots/" + "".join(random.choice(string.ascii_letters + string.digits) for i in range(32))
+            driver.execute_script('arguments[0].scrollIntoView({block: "center"});', element) # prevent cutting off
+            element.screenshot(path + ".png")
+            save_tts(text_element.text, path + ".wav")
 
-                # update cur comments
-                cur_comments.append((
-                    comment_text,
-                    path
-                ))
+            # add the new element to posts
+            new_element = {
+                "title": text_element.text[:92],
+                "link": text_element.get_attribute("href"),
+                "comments": [],
+                "thumbnail": path,
+            }
+            posts.append(new_element)
+        
+        # get top comments from posts
+        for idx, post in enumerate(posts):
 
-                # update post
-                if len(cur_comments) == max_comments:
-                    post["comments"].append(cur_comments.copy())
-                    cur_comments.clear()
+            # debug message
+            PRINTS(f"[DEBUG] Scraping post: {post['title']} ({idx + 1} / {len(posts)})")
 
-            except selenium.common.exceptions.WebDriverException: # deleted posts or haywire posts are skipped
-                pass
+            # change webpage
+            driver.get(post["link"])
+
+            # wait until enough comments are loaded
+            start_time = time.time()
+            comment_elems = []
+            while len(comment_elems) < max_videos * max_comments:
+                comment_elems = [
+                    element for element in
+                    driver.find_elements(By.CSS_SELECTOR, f"[class^='{COMMENT_ELEMENT_CLASS}']")
+                    if "level 1" in element.text # only get main posts
+                ]
+                if time.time() - start_time >= WAIT_TIMEOUT: break
+
+            # screenshot and store each comment
+            cur_comments = []
+            for element in comment_elems:
+
+                # exit if limit reached
+                if len(post["comments"]) == max_videos: break
+
+                try:
+                    # get comment as text
+                    text_element = element.find_element(By.XPATH, f".//div[@class='{COMMENT_TEXT_CLASS}']")
+                    if not text_element.text:
+                        continue
+
+                    # save screenshot and tts
+                    path = "screenshots/" + "".join(random.choice(string.ascii_letters + string.digits) for i in range(32))
+                    driver.execute_script('arguments[0].scrollIntoView({block: "center"});', element) # prevent cutting off
+                    element.screenshot(path + ".png")
+                    comment_text = filter_urls(text_element.text)
+                    save_tts(comment_text, path + ".wav")
+
+                    # update cur comments
+                    cur_comments.append((
+                        comment_text,
+                        path
+                    ))
+
+                    # update post
+                    if len(cur_comments) == max_comments:
+                        post["comments"].append(cur_comments.copy())
+                        cur_comments.clear()
+
+                except selenium.common.exceptions.WebDriverException: # deleted posts or haywire posts are skipped
+                    pass
+
+    except Exception as e:
+        move_chromedriver_file(False)
+        raise e
 
     move_chromedriver_file(False)
     return posts
@@ -195,83 +201,89 @@ def get_posts_PD(
     CHUNKSIZE=75, # chunk up long descriptions by chunks of n words
     WAIT_TIMEOUT=20, ignore=[], logging=True,
 ):
-    
-    # set logging switch
-    printswitch.switch.print = logging
-    
-    # just in case, create screenshots folder
-    try: os.mkdir("screenshots")
-    except FileExistsError: pass
-
-    # setup driver
-    driver = setup_driver(subreddit)
-
-    # wait until enough posts are loaded
-    start_time = time.time()
-    post_elems = []
-    while len(post_elems) < max_posts:
-        post_elems = [
-            element for element in
-            driver.find_elements(By.CSS_SELECTOR, f"[class*='{POST_ELEMENT_CLASS}']")
-            if not to_ignore(element, ignore)
-        ]
-        if time.time() - start_time >= WAIT_TIMEOUT: break
-
-    # find post: titles and descriptions
-    posts_to_visit = []
-    for element in post_elems:
-
-        # exit if limit reached
-        if len(posts_to_visit) == max_posts: break
-
-        # ads and anomaly skipper
-        try:
-            text_element = element.find_element(By.XPATH, f".//a[@class='{POST_TEXT_CLASS}']")
-        except selenium.common.exceptions.WebDriverException: # this should skip ads
-            continue
-
-        # save screenshot and tts
-        path = "screenshots/" + "".join(random.choice(string.ascii_letters + string.digits) for i in range(32))
-        driver.execute_script('arguments[0].scrollIntoView({block: "center"});', element) # prevent cutting off
-        element.screenshot(path + ".png")
-        save_tts(text_element.text, path + ".wav")
-
-        posts_to_visit.append((text_element.text, text_element.get_attribute("href"), path))
-
-    posts = []
-    for title, link, thumbnail in posts_to_visit:
-
-        # visit link
-        driver.get(link)
-
-        # get chunk text
-        PRINTS(f"[DEBUG]: Getting post description: {title}")
-        try:
-            description_text = driver.find_element(By.XPATH, f"//div[@class='{POST_DESCRIPTION_CLASS}']").text
-            if not description_text:
-                PRINTS(f"[DEBUG]: POST NO DESCRIPTION, ABORTING.")
-                return False # invalid post, subreddit might not work at all
-            description_text = description_text.split(" ")
-        except selenium.common.exceptions.NoSuchElementException:
-            PRINTS(f"[DEBUG]: POST NO DESCRIPTION, ABORTING.")
-            return False # same as above
         
-        # chunking up + tts
-        description_chunks = []
-        while description_text:
-            chunk = filter_urls(" ".join(description_text[:CHUNKSIZE]))
-            path = "screenshots/" + "".join(random.choice(string.ascii_letters + string.digits) for i in range(32))
-            save_tts(chunk, path + ".wav", False)
-            description_chunks.append((chunk, path))
-            description_text = description_text[CHUNKSIZE:]
+    try:
+    
+        # set logging switch
+        printswitch.switch.print = logging
+        
+        # just in case, create screenshots folder
+        try: os.mkdir("screenshots")
+        except FileExistsError: pass
 
-        # add the new element to posts
-        new_element = {
-            "title": title[:92],
-            "description_chunks": description_chunks,
-            "thumbnail": thumbnail,
-        }
-        posts.append(new_element)
+        # setup driver
+        driver = setup_driver(subreddit)
+
+        # wait until enough posts are loaded
+        start_time = time.time()
+        post_elems = []
+        while len(post_elems) < max_posts:
+            post_elems = [
+                element for element in
+                driver.find_elements(By.CSS_SELECTOR, f"[class*='{POST_ELEMENT_CLASS}']")
+                if not to_ignore(element, ignore)
+            ]
+            if time.time() - start_time >= WAIT_TIMEOUT: break
+
+        # find post: titles and descriptions
+        posts_to_visit = []
+        for element in post_elems:
+
+            # exit if limit reached
+            if len(posts_to_visit) == max_posts: break
+
+            # ads and anomaly skipper
+            try:
+                text_element = element.find_element(By.XPATH, f".//a[@class='{POST_TEXT_CLASS}']")
+            except selenium.common.exceptions.WebDriverException: # this should skip ads
+                continue
+
+            # save screenshot and tts
+            path = "screenshots/" + "".join(random.choice(string.ascii_letters + string.digits) for i in range(32))
+            driver.execute_script('arguments[0].scrollIntoView({block: "center"});', element) # prevent cutting off
+            element.screenshot(path + ".png")
+            save_tts(text_element.text, path + ".wav")
+
+            posts_to_visit.append((text_element.text, text_element.get_attribute("href"), path))
+
+        posts = []
+        for title, link, thumbnail in posts_to_visit:
+
+            # visit link
+            driver.get(link)
+
+            # get chunk text
+            PRINTS(f"[DEBUG]: Getting post description: {title}")
+            try:
+                description_text = driver.find_element(By.XPATH, f"//div[@class='{POST_DESCRIPTION_CLASS}']").text
+                if not description_text:
+                    PRINTS(f"[DEBUG]: POST NO DESCRIPTION, ABORTING.")
+                    return False # invalid post, subreddit might not work at all
+                description_text = description_text.split(" ")
+            except selenium.common.exceptions.NoSuchElementException:
+                PRINTS(f"[DEBUG]: POST NO DESCRIPTION, ABORTING.")
+                return False # same as above
+            
+            # chunking up + tts
+            description_chunks = []
+            while description_text:
+                chunk = filter_urls(" ".join(description_text[:CHUNKSIZE]))
+                path = "screenshots/" + "".join(random.choice(string.ascii_letters + string.digits) for i in range(32))
+                save_tts(chunk, path + ".wav", False)
+                description_chunks.append((chunk, path))
+                description_text = description_text[CHUNKSIZE:]
+
+            # add the new element to posts
+            new_element = {
+                "title": title[:92],
+                "description_chunks": description_chunks,
+                "thumbnail": thumbnail,
+            }
+            posts.append(new_element)
+
+    except Exception as e:
+        move_chromedriver_file(False)
+        raise e
 
     move_chromedriver_file(False)
     return posts
